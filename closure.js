99f0c0d86840ee797c04bf4463c4c837e9adbf9c
Shrikant Ramani <shrikantramani@Shrikants-MacBook-Air.local>
2024-07-22 13:55:03 +0530

    files added

--- closure.js
+++ closure.js
@@ -1,41 +1,41 @@
-// a closure is a function  having acces to the parent scope , even after the parent function has closed
+// // a closure is a function  having acces to the parent scope , even after the parent function has closed
 
-//a closure is created when we define a function, not when a func is executed
-
-
-//closure  --> a function bundled with its lexical environment is called closure, in innermost func , the func remembers ther reference of the variable.
+// //a closure is created when we define a function, not when a func is executed
 
 
-// A closure gives access to all the variables of it's parent function even after the that parent function has returned or executed. The function keeps a refernce to it's outer scope which preserves the scope chain throughout the time
-// function x(){
-//     let bub = 1;
+// //closure  --> a function bundled with its lexical environment is called closure, in innermost func , the func remembers ther reference of the variable.
 
-//     function y(){
-//         console.log(bub);
-//     }
-//     return y;
 
-// }
+// // A closure gives access to all the variables of it's parent function even after the that parent function has returned or executed. The function keeps a refernce to it's outer scope which preserves the scope chain throughout the time
+function x(){
+    let bub = 1;
 
-// var z = x()
-// console.log(z)
-// z()
+    function y(){
+        console.log(bub);
+    }
+    return y;
 
-//deep nested closure
+}
 
-// function z(){
-//     let a = 100;
-//     function y(){
-//         let b = 50
-//         return function x(){
-//             console.log(a,b)
-//         }
+var z = x()
+console.log(z)
+z()
+
+// //deep nested closure
+
+function z(){
+    let a = 100;
+    function y(){
+        let b = 50
+        return function x(){
+            console.log(a,b)
+        }
        
-//     }
-//     const yfunc = y();
-//     yfunc()
-// }
-// z();
+    }
+    const yfunc = y();
+    yfunc()
+}
+z();
 
 function outer(){
     let a = 100;
@@ -46,11 +46,11 @@ function outer(){
 const returnedfunc = outer()
 returnedfunc();
 
-//uses 
-- modules Design pattern
-- currying
--function like once
-- memoize
-- maintaining state in async world
-- setTimeouts
-- iterators
+// //uses 
+// // - modules Design pattern
+// // - currying
+// // -function like once
+// // - memoize
+// // - maintaining state in async world
+// // - setTimeouts
+// // - iterators
